2932439bb00773762b9a1e21c25bca4d5df43f0e
manuelperez <mperez14@correo.um.edu.uy>
2025-10-05 21:02:55 -0300

    Initial commit

---
+++ src/main/java/uy/um/edu/pizzum_burgum/PizzUmBurgUmApplication.java
@@ -0,0 +1,13 @@
+package uy.um.edu.pizzum_burgum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzUmBurgUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzUmBurgUmApplication.class, args);
+	}
+
+}

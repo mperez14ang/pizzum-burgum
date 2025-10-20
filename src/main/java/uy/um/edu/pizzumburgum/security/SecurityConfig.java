317e015ec87b3b9dd91c3552e58d986475996dcf
eldek <eduardo.santiagodelfante10@gmail.com>
2025-10-20 19:21:13 -0300

    Se cambio securityConfig y se cambio la consulta de products/ingredients

--- src/main/java/uy/um/edu/pizzumburgum/security/SecurityConfig.java
+++ src/main/java/uy/um/edu/pizzumburgum/security/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3001"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.addAllowedHeader("*");
